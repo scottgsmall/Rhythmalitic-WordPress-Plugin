0bff0907210baa579439ecde9a40f2a62657bd31
Scott Small <scottgsmall@comcast.net>
2016-03-15 13:10:21 -0700

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Rhythmalitic-WordPress-Plugin
+
 Skeleton for generic WordPress Plugin using OO design and MVC pattern.
---
+++ rhythmalitic-wordpress-plugin.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ Plugin Name: Rhythmalitic Wordpress Plugin
+ Plugin URI:  https://github.com/scottgsmall/Rhythmalitic-WordPress-Plugin.git
+ Description: Skeleton for generic WordPress Plugin using OO design and MVC pattern.
+ Version:     0.1
+ Author:      Scott Small
+ Author URI:  http://rhythmalitic.com
+ */
